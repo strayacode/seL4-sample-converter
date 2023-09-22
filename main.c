c783d6c07f70206446f42ce3549bc3837bf76829
strayacode <dallascuber@gmail.com>
2023-09-22 14:51:16 +1000

    add perf data structures

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include "perf.h"
+#include "sample.h"
 
 int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("usage: ./seL4-sample-convert <sample_file>\n");
+        return 1;
+    }
+
+    char *sample_file = argv[1];
+
     return 0;
 }
--- sample.h
+++ sample.h
@@ -1,21 +1,21 @@
 #ifndef SAMPLE_H
 #define SAMPLE_H
 
-#include <stdint.h>
+#include "types.h"
 
 // this file defines the format used for sel4 profiling packets
 
 #define CALL_STACK_DEPTH 10
 
 struct sel4_sample {
-    uint64_t ip; // instruction pointer
-    uint32_t pid; // process id
-    uint64_t time; // timestamp of when the sample occured
-    uint32_t cpu; // cpu affinity - which cpu is being used
-    uint64_t period; // number of events per sample
-    uint64_t ips[CALL_STACK_DEPTH]; // call stack - provides a trace of addresses for functions called
+    u64 ip; // instruction pointer
+    u32 pid; // process id
+    u64 time; // timestamp of when the sample occured
+    u32 cpu; // cpu affinity - which cpu is being used
+    u64 period; // number of events per sample
+    u64 ips[CALL_STACK_DEPTH]; // call stack - provides a trace of addresses for functions called
 };
 
-typedef sel4_sample sel4_sample_t;
+typedef struct sel4_sample sel4_sample_t;
 
 #endif
